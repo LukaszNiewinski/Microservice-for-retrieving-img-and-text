61cf43377f05c46baceb596eb44cd71e5d8eb2a4
Lukasz Niewinski <lukasz13n@gmail.com>
2020-02-19 18:08:04 +0100

    webpage form request

--- webpage/application.py
+++ webpage/application.py
@@ -1,6 +1,6 @@
 # Text retrieve service
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, jsonify
 from flask_restful import Resource, Api
 import requests
 
@@ -17,11 +17,19 @@ def home():
 
 @app.route('/retrieve_data', methods=['GET','POST'])
 def retrieve_data():
-    img_j = requests.get('http://img_retrieve').json()
+    if request.method == 'POST':
 
-    text_j = requests.get('http://text_retrieve').json()
+        # to get all the values from checkboxes it needs to be in dict format
+        response_dict = request.form.to_dict(flat=False)
+        to_retrieve = response_dict['retrieve']
+        url_path  = response_dict['url_path']
+        print(to_retrieve, url_path)
 
-    return render_template('retrieve_data.html')
+
+        return response_dict
+    else:
+        return render_template('retrieve_data.html')
+
 
 @app.route('/get_data', methods=['GET','POST'])
 def get_data():
