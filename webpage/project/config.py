ac315a4b07fa5473bcc3a0013ec48cc1ead118b8
Lukasz Niewinski <lukasz13n@gmail.com>
2020-03-03 12:30:29 +0100

    changed db configuration

--- webpage/project/config.py
+++ webpage/project/config.py
@@ -7,5 +7,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    # SQLALCHEMY_DATABASE_URI = "postgresql://hello_semantive:hire_me@db:5432/retrieve_data_dev"
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://")
